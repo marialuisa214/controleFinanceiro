2f0dcefc0a6e15ad53e5eac5b1e0814d0575e996
Maria Luisa <211039617@aluno.unb.br>
2024-01-05 15:14:17 -0300

    :nail_care: feat: componente Header

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@
 import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/themes/default'
 import { GlobalStyle } from './styles/global'
+import { Trasaction } from './pages/Transactions'
 
 
 export function App() {  
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
-      <h1>Hello World</h1>
+      <Trasaction />
     </ThemeProvider>
   )
 }
---
+++ src/pages/Transactions/index.tsx
@@ -0,0 +1,10 @@
+import { Header } from "../../components/Header"
+
+export function Trasaction() {
+    return (
+        <div>
+            <Header />
+            <h1>Transactions</h1>
+        </div>
+    )
+}
