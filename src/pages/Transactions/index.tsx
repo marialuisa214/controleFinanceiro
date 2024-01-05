78bff4e31c9acc233c05236cc775ee6613cf34f3
Maria Luisa <211039617@aluno.unb.br>
2024-01-05 18:21:32 -0300

    :nail_care: feat: tabela de transações

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,12 +1,37 @@
 import { Header } from "../../components/Header"
 import { Summary } from "../../components/Summary"
+import { PriceHighLight, TransactionContainer, TransactionTable } from "./styles"
+
 
 export function Trasaction() {
+    const  itensTable = ['outcome','income','outcome','income','income', 'outcome']
     return (
         <div>
             <Header />
             <Summary />
-            <h1>Transactions</h1>
+            <TransactionContainer>
+                <TransactionTable>
+                    <tbody>
+                        {(itensTable).map((item) => (
+                            <tr key={item}>
+                                <td width="40%">Desenvolvimento de website</td>
+                                <td className="deposit">
+                                    <PriceHighLight variant={item === 'income' ? 'income' : 'outcome'}>
+                                        R$ 12.000
+                                    </PriceHighLight>
+                                </td>
+                                <td>Desenvolvimento</td>
+                                <td>12/02/2021</td>
+                            </tr>
+                        ))
+                    }
+                        
+                    </tbody>
+                </TransactionTable>
+            </TransactionContainer>
+            
+
+
 
         </div>
     )
